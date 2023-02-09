baa31df8f9b3a0682553965a1b516875a1a0b71e
mr-chacha <hoitchac@gmail.com>
2023-02-09 10:02:03 +0900

    fix 회원가입수정중

---
+++ firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAP6q65SrEjAxSQ4YRZuYhN-j8MT6bE1p0",
+  authDomain: "steamplus-a629d.firebaseapp.com",
+  projectId: "steamplus-a629d",
+  storageBucket: "steamplus-a629d.appspot.com",
+  messagingSenderId: "303570818411",
+  appId: "1:303570818411:web:31f08066343b189f4b682e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const apiKeys = firebaseConfig.apiKey;
+export const db = getFirestore(app);
+export const authService = getAuth(app);
+export const storage = getStorage(app);
+
+export default app;
--- src/components/signuppage/Signup.tsx
+++ src/components/signuppage/Signup.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 function Signup() {
-  return <div>회원가입</div>;
+  //닉네임인풋
+  const [nicName, setNicName] = useState();
+  const nicNameRef = useRef<HTMLInputElement>(null);
+  const nicNameOnChange = (event: any) => {
+    setNicName(event.target.value);
+  };
+  //아이디 인풋
+  const [userId, setUserId] = useState();
+  const userIdRef = useRef<HTMLInputElement>(null);
+  const userIdOnChange = (event: any) => {
+    setUserId(event.target.value);
+  };
+  //패스워드 인풋
+  const [userFw, setUserFw] = useState();
+  const userFwRef = useRef<HTMLInputElement>(null);
+  const userFwOnChange = (event: any) => {
+    setUserFw(event.target.value);
+  };
+
+  return (
+    <>
+      <h1>회원가입</h1>
+      <form>
+        <section>
+          닉네임 :{" "}
+          <input value={nicName} onChange={nicNameOnChange} ref={nicNameRef} />
+          이메일 :{" "}
+          <input value={userId} onChange={userIdOnChange} ref={userIdRef} />
+          비밀번호 :{" "}
+          <input value={userFw} onChange={userFwOnChange} ref={userFwRef} />
+          <button>확인</button>
+        </section>
+      </form>
+    </>
+  );
 }
 
 export default Signup;
