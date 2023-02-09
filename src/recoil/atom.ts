d674b61cc25c71cdbd24b5799b342651c0c3d64e
prdg <prdg990515@gmail.com>
2023-02-09 21:31:05 +0900

    fix 다른방식으로 메뉴 구현

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,23 +1,6 @@
-import React from "react";
 import { atom } from "recoil";
 
-export const profilebutton = atom({
-  key: "profile",
-  default: false,
-});
-export const gamesearchbutton = atom({
-  key: "gamesearchbutton",
-  default: false,
-});
-export const friendbutton = atom({
-  key: "friendbutton",
-  default: false,
-});
-export const friendsearchbutton = atom({
-  key: "friendsearchbutton",
-  default: false,
-});
-export const voicetalkbutton = atom({
-  key: "voicetalkbutton",
-  default: false,
+export const LayoutButton = atom<String>({
+  key: "Layout",
+  default: "close",
 });
