7913843ab8b222f3e36c7a87b37dca4fd3cf4990
freedobby77 <117061022+freedobby77@users.noreply.github.com>
2023-02-09 18:01:06 +0900

    게임검색 기능ing

--- src/pages/ChannelSearchPage.tsx
+++ src/pages/ChannelSearchPage.tsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+interface Game {
+  [key: string]: string | number | boolean | any;
+  appid: number;
+  name: string;
+}
 
 const ChannelSearchPage = () => {
-  return <div>ChannelSearchPage</div>;
+  const SteamSearch = () => {
+    const [searchGames, setSearchGames] = useState<Game[]>([]);
+    // const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<any>(null);
+    const API_KEY = "366F82432C264B4FB1CE15534A050D09";
+    const [searchInput, setSearchInput] = useState("");
+
+    const searchSteamGames = async () => {
+      // setLoading(true);
+      setError(null);
+      try {
+        const response = await axios.get<{ applist: { apps: Game[] } }>(
+          `http://api.steampowered.com/ISteamApps/Search/v1/?key=${API_KEY}&query=${searchInput}`
+        );
+        setSearchGames(response.data.applist.apps);
+      } catch (error) {
+        setError(error);
+      }
+      // setLoading(false);
+    };
+
+    useEffect(() => {
+      searchSteamGames();
+    }, [searchInput]);
+
+    // const [inputText, setInputText] = useState("");
+    // const BASE_SEARCH_URL = "";
+    // const API_KEY = "366F82432C264B4FB1CE15534A050D09";
+
+    // const getSearchGames = async () => {};
+
+    return (
+      <div>
+        <input
+          type="text"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button onClick={searchSteamGames}>Search</button>
+
+        {searchGames.map((game: Game) => (
+          <div key={`${game.appid}`}>
+            <div>{`${game.name}`}</div>
+          </div>
+        ))}
+        {/* {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        <ul>
+          {games.map((game) => (
+            <li key={game.appid}>{game.name}</li>
+          ))}
+        </ul>
+      )} */}
+      </div>
+    );
+  };
 };
 
 export default ChannelSearchPage;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ChannelSearchPage from "../pages/ChannelSearchPage";
+import MainPage from "../pages/MainPage";
 
 const Router = () => {
-  return <div>Router</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/search" element={<ChannelSearchPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
