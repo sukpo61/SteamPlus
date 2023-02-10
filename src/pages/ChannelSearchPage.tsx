0dff41a0d08515b27cdd48fb81b79b75a1ebf692
go <sukpo6010@gmail.com>
2023-02-10 20:50:02 +0900

    api 진행중

--- src/pages/ChannelSearchPage.tsx
+++ src/pages/ChannelSearchPage.tsx
@@ -13,6 +13,12 @@ const ChannelSearchPage: any = () => {
   // const [loading, setLoading] = useState(false);
   const [error, setError] = useState<any>(null);
   const [searchInput, setSearchInput] = useState("dead");
+  const APIKEY = "234E0113F33D5C7C4D4D5292C6774550";
+  const STEAM_ACCOUNT_NAME = "sukpo61@naver.com";
+  const client_id = "ucj588lq839jowdgqawe2651hbna80";
+  const client_secret = "ie8i79maa233egjs88c90el20svaav";
+  const token = "1cly6ag9tr1y0q3mvvck9jw1j123sc";
+  const gameId = "87173";
 
   useEffect(() => {
     // axios
@@ -27,42 +33,109 @@ const ChannelSearchPage: any = () => {
     //     }
     //   )
     //   .then((response) => {
-    //     console.log(response);
+    //     console.log("searchresult", response);
     //   })
     //   .catch((error) => {
     //     console.error(error);
     //   });
 
+    // axios
+    //   .get(
+    //     "https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/",
+    //     {
+    //       params: {
+    //         key: APIKEY,
+    //         steamids: "76561198374391933",
+    //       },
+    //     }
+    //   )
+    //   .then((response) => {
+    //     console.log("userstate", response);
+    //   })
+    //   .catch((error) => setError(error));
+
+    // axios
+    //   .get(
+    //     `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/`,
+    //     {
+    //       params: {
+    //         key: APIKEY,
+    //         vanityurl: "SUKPO61",
+    //       },
+    //     }
+    //   )
+    //   .then((response) => {
+    //     console.log("steamid", response);
+    //   })
+    //   .catch((error) => setError(error));
+
+    // axios
+    //   .get(
+    //     `https://cors-anywhere.herokuapp.com/http://store.steampowered.com/api/appdetails/`,
+    //     {
+    //       params: {
+    //         appids: "250900",
+    //       },
+    //     }
+    //   )
+    //   .then((response) => {
+    //     console.log("gamedetail", response);
+    //   })
+    //   .catch((error) => {
+    //     setError("Could not retrieve header image");
+    //   });
+
     axios
-      .get(
-        "https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/",
-        {
-          params: {
-            key: "234E0113F33D5C7C4D4D5292C6774550",
-            steamids: "76561198374391933",
-          },
-        }
+      .post(
+        `https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=client_credentials`
       )
       .then((response) => {
-        console.log(response);
+        console.log("token", response);
       })
-      .catch((error) => setError(error));
+      .catch((error) => {
+        setError("Could not retrieve header image");
+      });
+
+    axios({
+      url: "https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/screenshots",
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Client-ID": "ucj588lq839jowdgqawe2651hbna80",
+        Authorization: `Bearer eca8nhousl74mt7g1gndeek0dd6b9e`,
+      },
+      // params: {
+      //   fields: "url",
+      //   filter: `[games][eq]=${gameId}&[resolution][eq]=720p`,
+      // },
+
+      // data: "fields alpha_channel,animated,checksum,game,height,image_id,url,width;",
+      data: {
+        fields: {
+          url: {},
+          game: {},
+        },
+        filter: {
+          game: {
+            eq: gameId,
+          },
+          resolution: {
+            eq: "720p",
+          },
+        },
+      },
+    })
+      .then((response) => {
+        console.log("image", response);
+      })
+      .catch((error) => {
+        console.log("error", error);
+
+        // setError("Could not retrieve header image");
+      });
   }, []);
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-      />
-      {searchGames.map((game: Game) => (
-        <div key={`${game.appid}`}>
-          <div>{`${game.name}`}</div>
-        </div>
-      ))}
-    </div>
-  );
+  return <div>1</div>;
 };
 
 export default ChannelSearchPage;
