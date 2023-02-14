7f497cd06357d20064ff6a8a3293d775248a1205
go <sukpo6010@gmail.com>
2023-02-14 17:25:33 +0900

    feat: 로그인 처리

--- src/pages/ChannelSearchPage.tsx
+++ src/pages/ChannelSearchPage.tsx
@@ -1,23 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Loginedrecoil } from "../recoil/atom";
+import { useRecoilState } from "recoil";
+import { useParams } from "react-router";
 
 //스팀 로그인 페이지 변수
-const STEAM_OPENID_ENDPOINT = `https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=http://localhost:3000/&openid.realm=http://localhost:3000&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select`;
+const STEAM_OPENID_ENDPOINT = `https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=http://localhost:3001/&openid.realm=http://localhost:3001&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select`;
+
 const ChannelSearchPage: any = () => {
+  const params: any = new URLSearchParams(window.location.search);
+  const steamId = params.get("openid.claimed_id")?.split("/")[5];
+  console.log(steamId);
+
   const [error, setError] = useState(null);
   const APIKEY = "234E0113F33D5C7C4D4D5292C6774550";
+
+  const [userinfo, setUserinfo] = useState({
+    id: "",
+    profileimg: "",
+    nickname: "",
+  });
+
   const SteamLogin = () => {
     window.location.href = STEAM_OPENID_ENDPOINT;
   };
-  // https://cors-anywhere.herokuapp.com/
-  // steam nickName
-  const [nickName, setNickName] = useState();
-  // steam profileImg URL
-  const [profileImg, setProfileImg] = useState();
-  //steam id
 
-  const [steamids, setSteamids] = useState<any>("76561199476963056");
-  const [test, setTest] = useState<any>();
+  // const [steamids, setSteamids] = useState<any>("76561199476963056");
+  // const [test, setTest] = useState<any>();
   // ___76561199478670991,
   // __76561199476963056;
   //steam id를 slice로 잘라서 아이디 고유번호로 쓸거
@@ -26,60 +35,33 @@ const ChannelSearchPage: any = () => {
   // const userNicName = nickName + "#" + hashtag;
 
   const userData = async () => {
-    const { data } = await axios.get(
-      "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/",
-      {
+    axios
+      .get("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/", {
         params: {
           key: APIKEY,
           //스팀로그인
-          steamids,
+          steamids: steamId,
         },
-      }
-    );
-    setTest(data);
-    // setSteamids(data.response.players[0].personaname);
+      })
+      .then((response) => {
+        console.log("userstate", response);
+
+        setUserinfo({
+          id: response.config.params.steamids,
+          profileimg: response?.data.response.players[0].avatarfull,
+          nickname: response?.data.response.players[0].personaname,
+        });
+        console.log("data", userinfo);
+      })
+      .catch((error) => setError(error));
   };
-  const testName = test?.response.players[0].personaname;
-  const testImg = test?.response.players[0].avatarfull;
-  const testId = test?.response.players[0].steamid;
+
   // setSteamids(testId);
-  console.log(testName, testImg, testId);
 
   useEffect(() => {
-    userData();
-    //유저정보
-    //cors 에러 헤결 https://cors-anywhere.herokuapp.com 들어가서 request 누르기
-    // axios
-    //   .get("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/", {
-    //     params: {
-    //       key: APIKEY,
-    //       //스팀로그인
-    //       steamids: "76561199478670991",
-    //     },
-    //   })
-    //   .then((response) => {
-    //     console.log("userstate", response);
-    //     setNickName(response?.data.response.players[0].personaname);
-    //     setProfileImg(response?.data.response.players[0].avatarfull);
-    //     setSteamids(response.config.params.steamids);
-    //   })
-    //   .catch((error) => setError(error));
-    // 게임정보
-    // axios
-    //   .get(
-    //     `https://cors-anywhere.herokuapp.com/http://store.steampowered.com/api/appdetails/`,
-    //     {
-    //       params: {
-    //         appids: "250900",
-    //       },
-    //     }
-    //   )
-    //   .then((response) => {
-    //     console.log("gamedetail", response);
-    //   })
-    //   .catch((error) => {
-    //     setError("Could not retrieve header image");
-    //   });
+    if (steamId) {
+      userData();
+    }
   }, []);
 
   return (
