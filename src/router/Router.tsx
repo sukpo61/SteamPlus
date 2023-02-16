cdacd1596a4555fce0b586fdd94704d95151dbd5
mr-chacha <hoitchac@gmail.com>
2023-02-16 17:26:35 +0900

    SangHyun-Signup

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -8,25 +8,21 @@ import Login from "../pages/LoginPage";
 import MyPage from "../pages/MyPage";
 import TeamChat from "../pages/TeamChat";
 import GlobalStyles from "../GlobalStyles";
-import { CookiesProvider } from "react-cookie";
+
 const Router = () => {
   return (
-    <CookiesProvider>
-      <BrowserRouter>
-        <GlobalStyles />
-        <Routes>
-          {/* <Route path="/channelsearchpage" element={<MainPage />} /> */}
-          {/* <Route path="/" element={<ChannelSearchPage />} /> */}
-          <Route path="/findaccountpage" element={<FindAccountPage />} />
-          <Route path="/findpasswordpage" element={<FindPasswordPage />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/teamchat" element={<TeamChat />} />
-        </Routes>
-      </BrowserRouter>
-    </CookiesProvider>
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/findaccountpage" element={<FindAccountPage />} />
+        <Route path="/findpasswordpage" element={<FindPasswordPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/teamchat" element={<TeamChat />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
