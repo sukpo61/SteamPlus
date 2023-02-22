ec322f3eaaad4903e801fd56e16090b31adeaf5a
mr-chacha <hoitchac@gmail.com>
2023-02-22 10:55:45 +0900

    Fix 정근님꺼랑 합체

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -68,44 +68,40 @@
 
 //   //steam userData
 //   const { data } = useQuery("userData", userDataGet);
+//   console.log("dat1a", data?.data.response.players[0].steamid);
+
 //   const userID = data?.data.response.players[0].steamid;
+// console.log("userID",userID);
 
 //   //dbjson에 유저정보 삭제하기 핸들러
-//   const userDeleteMutation = useMutation(() =>
-//     axios.delete(`${serverUrl}${userID}`)
-//   );
-//   const deleteHandeler = () => {
-//     userDeleteMutation.mutate();
-//     alert("회원탈퇴 되었습니다");
-//     navigate(`/`);
-//   };
-//   const goto = () => {
-//     navigate("/mypage");
-//   };
-//   useEffect(() => {
-//     if (steamId) {
-//       userDataGet();
-//     }
-//   }, []);
+//   // const userDeleteMutation = useMutation(() =>
+//   //   axios.delete(`${serverUrl}${userID}`)
+//   // );
+//   // const deleteHandeler = () => {
+//   //   userDeleteMutation.mutate();
+//   //   alert("회원탈퇴 되었습니다");
+//   //   navigate(`/`);
+//   // };
+//   // const goto = () => {
+//   //   navigate("/mypage");
+//   // };
+//   // useEffect(() => {
+//   //   if (steamId) {
+//   //     userDataGet();
+//   //   }
+//   // }, []);
 
 //   return (
 //     <div style={{ background: "white" }}>
 //       <button onClick={SteamLogin}>Login with Steam</button>
-//       <button onClick={deleteHandeler}>회원탈퇴하기</button>
-//       <button onClick={goto}>클릭</button>
+//       {/* <button onClick={deleteHandeler}>회원탈퇴하기</button> */}
+//       {/* <button onClick={goto}>클릭</button> */}
 //     </div>
 //   );
 // };
 
-// export default LoginPage;
-// import React from "react";
-
-// function LoginPage() {
-//   return <div>LoginPage</div>;
-// }
-
 export default LoginPage;
 
-export const LoginPage = () => {
+function LoginPage() {
   return <div>LoginPage</div>;
-};
+}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,6 +5,11 @@ import { PoularChannel } from "../components/PoularChannel";
 import { CurrentGame } from "../components/CurrentGame";
 import { useQuery } from "react-query";
 function MainPage() {
+  const APIKEY = "234E0113F33D5C7C4D4D5292C6774550";
+  const GameId: any = sessionStorage.getItem("gameid");
+
+  const GameIds: any =
+    GameId === "undefined" ? 990080 : sessionStorage.getItem("gameid");
   //게임이미지 불러오기
   const Gamedata = async () => {
     const response = await axios.get(
@@ -12,23 +17,25 @@ function MainPage() {
       {
         params: {
           // appids: GameId,
-          appids: 990080, // 해당 게임의 id값
+          appids: GameIds, // 해당 게임의 id값
         },
       }
     );
+
     return {
-      gametitle: response.data[990080].data.name,
-      gameCategories: response.data[990080].data.genres[0].description,
-      gameCategories2: response.data[990080].data.genres[1].description,
-      gameCategories3: response.data[990080].data.genres[2].description,
-      gameMainImg: response.data[990080].data.screenshots[1].path_full,
-      gameSubimg: response.data[990080].data.header_image,
+      gametitle: response.data[GameIds].data.name,
+      gameCategories: response.data[GameIds].data.genres[0].description,
+      gameCategories2: response.data[GameIds].data.genres[1].description,
+      gameCategories3: response.data[GameIds].data.genres[2].description,
+      gameMainImg: response.data[GameIds].data.screenshots[1].path_full,
+      gameSubimg: response.data[GameIds].data.header_image,
     };
     // return response
   };
 
-  const { data } = useQuery("Gamedata", Gamedata);
+  const { data }: any = useQuery("Gamedata", Gamedata);
   // console.log("data2", data);
+  // 새로고침시 업데이트하기
 
   return (
     <MainLayout>
