d9e83c61750a534527e4599a9a765289d6c950d2
prdg <prdg990515@gmail.com>
2023-03-02 18:30:36 +0900

    add import 추가

--- src/layout/layoutcomponents/VoiceTalk.jsx
+++ src/layout/layoutcomponents/VoiceTalk.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { LayoutButton } from "../../recoil/atom";
+import { getFriend, LayoutButton } from "../../recoil/atom";
 import styled from "styled-components";
 import { useRecoilValue } from "recoil";
 import socket from "../../socket";
